755aa3ba7fd9789374db643f885e1c0ab6bcc405
Phanord Picsou Roberto <www.phanordpicsouroberto11@gmail.com>
2025-03-30 12:34:26 -0400

    Initial commit: Simple ASP.NET Core API for summing two numbers

---
+++ SumApi.Tests/SumControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using SumApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SumApi.Tests
+{
+    public class SumControllerTests
+    {
+        [Fact]
+        public void Add_ReturnCorrectSum()
+        {
+            var controller = new SumController();
+
+            var result = controller.Add(12, 33) as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(45, result.Value);
+        }
+    }
+}
+
---
+++ SumApi/Controllers/SumController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SumApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SumController : ControllerBase
+    {
+        [HttpGet("add")]
+        public IActionResult Add(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + secondNumber;
+
+            return Ok(result);
+        }
+    }
+}
+
